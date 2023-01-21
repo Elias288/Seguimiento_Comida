859386db060a44b2f71821a70a525a084888498b
Elias Bianchi Hernandez <elias.bianchi@sofka.com.co>
2023-01-21 19:12:34 -0300

    preparando para el deploy

--- frontend/src/app/services/menu/menu.service.ts
+++ frontend/src/app/services/menu/menu.service.ts
@@ -1,38 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Menu } from 'src/app/utils/menu.inteface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class MenuService {
-    ENDPOINT = 'http://localhost:8080/api'
     constructor(
         private http: HttpClient,
-    ) { }
+    ) { 
+        // console.log('production:', environment.production)
+    }
 
     public create(menuData: Menu, jwt: string){
         const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${jwt}` }
         const body = JSON.stringify(menuData)
-        // console.log(body);
-        
-        return this.http.post(this.ENDPOINT + '/menu', body, { 'headers': headers })
+        return this.http.post(environment.apiUrl + '/menu', body, { 'headers': headers })
     }
 
     public getAllMenues() {
         const headers = { 'Content-Type': 'application/json' }
-        return this.http.get(this.ENDPOINT + '/menu', { 'headers': headers })
+        return this.http.get(environment.apiUrl + '/menu', { 'headers': headers })
     }
 
     public updateMenu(jwt: string, menu: Menu) {
         const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${jwt}` }
         const body = JSON.stringify( menu )
-        return this.http.put(this.ENDPOINT + '/menu', body, { 'headers': headers })
+        return this.http.put(environment.apiUrl + '/menu', body, { 'headers': headers })
     }
 
     public deleteMenu(menuId: string, jwt: string) {
         const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${jwt}` }
         const body = JSON.stringify({ id: menuId})
-        return this.http.delete(this.ENDPOINT + `/menu/${menuId}`, { 'headers': headers })
+        return this.http.delete(environment.apiUrl + `/menu/${menuId}`, { 'headers': headers })
     }
 }
--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -2,55 +2,57 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../../utils/user.interface'
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
-    ENDPOINT = 'http://localhost:8080/api'
 
     constructor(
         private http: HttpClient,
-    ) { }
+    ) { 
+        // console.log('production:', environment.production)
+    }
 
     public create(userData: User): Observable<any>  {
         const headers = { 'Content-Type': 'application/json' }
         const body = JSON.stringify(userData)
-        return this.http.post(this.ENDPOINT + '/user', body, { 'headers': headers })
+        return this.http.post(environment.apiUrl + '/user', body, { 'headers': headers })
     }
     
     public login(email: String, password: String): Observable<Object> {
         const headers = { 'Content-Type': 'application/json' }
         const body = { email, password }
-        return this.http.post(this.ENDPOINT + '/user/login', body, { 'headers': headers })
+        return this.http.post(environment.apiUrl + '/user/login', body, { 'headers': headers })
     }
     
     public getMe(jwt: String) {
         const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${jwt}` }
-        return this.http.get(this.ENDPOINT + '/user/me', { 'headers': headers })
+        return this.http.get(environment.apiUrl + '/user/me', { 'headers': headers })
     }
 
     public getAll(jwt: string) {
         const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${jwt}` }
-        return this.http.get(this.ENDPOINT + '/user', { 'headers': headers })
+        return this.http.get(environment.apiUrl + '/user', { 'headers': headers })
     }
 
     public addRole(jwt: string, userId: string, roles: string) {
         const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${jwt}` }
         const body = { userId, roles }
-        return this.http.put(this.ENDPOINT + '/user/addRoles', body, { 'headers': headers })
+        return this.http.put(environment.apiUrl + '/user/addRoles', body, { 'headers': headers })
     }
     
     public addToMenu(jwt: string, menuId: string, selectedMenu: string) {
         const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${jwt}` }
         const body = { menuId, selectedMenu }
-        return this.http.post(this.ENDPOINT + '/user/menu', body, { 'headers': headers })
+        return this.http.post(environment.apiUrl + '/user/menu', body, { 'headers': headers })
     }
 
     public removeToMenu(jwt: string, menuId: string) {
         const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${jwt}` }
         const body = { menuId }
-        return this.http.delete(this.ENDPOINT + '/user/menu/' + menuId, { 'headers': headers })   
+        return this.http.delete(environment.apiUrl + '/user/menu/' + menuId, { 'headers': headers })   
     }
 
     public logout(): void {
