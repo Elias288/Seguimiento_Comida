01d8a58f9ba49d8379abfd4f208db3c5532a865f
Elias Bianchi Hernandez <elias.bianchi@sofka.com.co>
2023-05-11 14:34:44 -0300

    Notificacion ID y token. Close #71
    Se verifica usuario conectado antes de enviar notificación.
    Se extendio el tiempo de expiracion de token.
    Se muestra error al expirar sesion.

--- backend/src/middleware/handleErrors.js
+++ backend/src/middleware/handleErrors.js
@@ -1,5 +1,5 @@
 const AppError = require("./AppError");
-const { TOKEN_EXPIRED } = require("./errorCodes");
+const { TOKEN_EXPIRED, ACCESS_DENIED } = require("./errorCodes");
 
 module.exports = (error, req, res, next) => {
     console.error(`[${new Date().toLocaleString('es-US', { timeZone: 'America/Montevideo', hour12: false })}]`, error);
@@ -16,13 +16,8 @@ module.exports = (error, req, res, next) => {
 }
 
 const ERROR_HANDLERS = {
-    // TokenExpiredError: (res, error) => res.status(400).send({errorCode: TOKEN_EXPIRED, details: "Token expirado"}),
-    // JsonWebTokenError: (res, error) => res.status(400).send({ error: error.name, details: error.details }),
-
-    // SequelizeValidationError: (res, error) => res.status(400).send({ error: error.name, details: error.errors[0].details}),
-    // SequelizeUniqueConstraintError: (res, error) => res.status(400).send({ error: error.name, details: error.errors[0].details}),
-
-    // ValidationError: (res, error) => res.status(400).send({ type: 'ValidationError', details: error.details}),
+    TokenExpiredError: (res, error) => res.status(400).send({ errorCode: TOKEN_EXPIRED, details: "Sesión expirada" }),
+    JsonWebTokenError: (res, error) => res.status(400).send({ errorCode: ACCESS_DENIED, details: "Token invalido" }),
     
     defaultError: (res) => { res.status(500).send('Something went wrong') }
 }
